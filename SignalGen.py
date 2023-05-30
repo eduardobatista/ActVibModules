9ed03b02a096eb6ecf317153cb4ae31422f4d3a7
Eduardo Batista <dudubatista@gmail.com>
2023-05-30 11:06:09 -0300

    Comments on chirp signal generation

--- SignalGen.py
+++ SignalGen.py
@@ -19,26 +19,41 @@ def filteredNoise(Npoints,var,lowcut,highcut,samplingFreq,order=10):
     x = lfilter(b, a, xa)
     return x
 
-def chirp(seconds,samplingfreq,freq,ampl=1,deltai=10,deltaf=10,tinicio=10,tfim=None,alpha=0.9):
+def chirp(seconds,samplingfreq,freq,ampl=1,deltai=10,deltaf=10,tinicio=10,tfim=None):
+    """
+    Generates a signal that resembles the start, steady state and end of the functioning of a rotating machine.
+    Such a signal is useful for testing active vibration control systems.
+    The generated signal is based on the chirp signal.
+
+    Args:
+        seconds (int): Total time of the signal
+        samplingfreq (int): Sampling frequency (Hz)
+        freq (int): Signal frequency (Hz)
+        ampl (int, optional): Signal amplitude at steady state. Defaults to 1.
+        deltai (int, optional): Rising time of the signal. Defaults to 10.
+        deltaf (int, optional): Fall time of the signal. Defaults to 10.
+        tinicio (int, optional): Time of the beginning of signal rise. Defaults to 10.
+        tfim (int, optional): Time of the beginning of signal fall. Defaults to None.
+
+    Returns:
+        numpy.ndarray: Generated signal
+    """    
     dt = 1/samplingfreq
     tt = np.arange(0,seconds,dt) # Vetor de tempo de 0 a Nseconds com passos de dt
     out = np.zeros(tt.shape[0]) # Retorno
     f = 0 # Freq. inicial = 0
-    integ = 0 # Integral inicialmente = 0
+    integ = 0 # Integral, valor inicial
     cA = ampl/(freq**2) # Constante para amplitude ficar igual a "ampl" em regime
-    A = 0 # Amplitude inicial = 9
+    A = 0 # Amplitude inicial
     if tfim is None:
         tfim = seconds
     for n,t in enumerate(tt): # Varia n de 0 ao número de elementos no vetor tt, e t = tt[n]
         if (t < tinicio) or (t > (tfim+deltaf)):
-            f = 0
-            #A = 0                
+            f = 0             
         elif t <= (tinicio+deltai):
-            f = freq * (t-tinicio) / deltai
-            #A = ampl * (t-tinicio) / deltat                
+            f = freq * (t-tinicio) / deltai              
         elif (t >= tfim) and (t <= (tfim+deltaf)):
-            f = freq - freq * (t-tfim) / deltaf
-            #A = ampl - ampl * (t-tfim) / deltat                
+            f = freq - freq * (t-tfim) / deltaf               
         A = (f**2) * cA
         integ = integ + 2*np.pi*f*dt
         out[n] = A * np.sin(integ)
